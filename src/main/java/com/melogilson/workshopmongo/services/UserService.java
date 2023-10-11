a5f1ddb9c89f2516c18619d85f400ba7bc93875d
GilsonMelo3638 <gilson.melo@gdfnet.df.gov.br>
2023-10-11 14:31:52 -0300

    Obtendo um usuário por id.

--- src/main/java/com/melogilson/workshopmongo/services/UserService.java
+++ src/main/java/com/melogilson/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.melogilson.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.melogilson.workshopmongo.domain.User;
 import com.melogilson.workshopmongo.repository.UserRepository;
+import com.melogilson.workshopmongo.services.exception.ObjectNotFoundException;
 
 
 
@@ -19,4 +21,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
