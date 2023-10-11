cee8d5f065f1c388b1a64cf26f1723b2ec7fd966
GilsonMelo3638 <gilson.melo@gdfnet.df.gov.br>
2023-10-11 20:19:48 -0300

    Projeção dos dados do autor com DTO.

--- src/main/java/com/melogilson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/melogilson/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.melogilson.workshopmongo.domain.Post;
 import com.melogilson.workshopmongo.domain.User;
+import com.melogilson.workshopmongo.dto.AuthorDTO;
 import com.melogilson.workshopmongo.repository.PostRepository;
 import com.melogilson.workshopmongo.repository.UserRepository;
 
@@ -37,11 +38,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
-
-}
+}	
