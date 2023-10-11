86d04b86b2f7cc63c4122436f85b87046f78fcb6
GilsonMelo3638 <gilson.melo@gdfnet.df.gov.br>
2023-10-11 10:38:16 -0300

    Conectando ao MongoDB com repository e service.

---
+++ src/main/java/com/melogilson/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.melogilson.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.melogilson.workshopmongo.domain.User;
+import com.melogilson.workshopmongo.repository.UserRepository;
+
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
